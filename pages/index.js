10d0c98d5b1b8ee528256a66092874e2362b4649
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-12 22:25:20 -0300

    MB. Aula 24 - Parte 01 - Problema na API

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
 
+
+import Card from '@/components/Card';
+
 export async function getStaticProps(){
   const maxPokemons = 50;
   const api = "http://pokeapi.co/api/v2/pokemon/";
@@ -30,7 +33,7 @@ export default function Home({pokemons}) {
       </div>
       <div className={styles.pokemon_container}>
         {pokemons.map((pokemon)=>(
-          <p key={pokemon.id}>{pokemon.name}</p>
+          <Card key={pokemon.id} pokemon={pokemon}/>
         ))}
       </div>
     </>
